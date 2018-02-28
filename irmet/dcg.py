58dde53b5680b8a979844f55acc809641cfaa186
moriaki3193 <puppis1221@gmail.com>
2018-02-28 22:23:58 +0900

    add auc.py & modify dcg.py

--- irmet/dcg.py
+++ irmet/dcg.py
@@ -1,54 +1,79 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from functools import reduce
-from operator import add
+
+
+def _calc_numer(rels):
+    """Calculate numerator of DCG
+
+    params
+    ------
+    rels :
+        np.ndarray, shape = (n_entities, )
+
+    return
+    ------
+    numer :
+        np.ndarray, The numerator of DCG
+    """
+    return np.array([2.0 ** rel - 1 for rel in rels])
+
+
+def _calc_denom(n_entities):
+    """Calculate denominator of DCG
+
+    params
+    ------
+    n_entities :
+        int, # of entities.
+
+    return
+    ------
+    denom :
+        np.ndarray, The denominator of DCG
+    """
+    return np.log2(np.arange(2, n_entities + 2))
+
 
 
 def DCG(rels):
-    """DCGを計算する関数.
+    """Calculate DCG based on given relevances of entities.
 
-    arguments
-    ---------
-    rels : array-like, shape (n, )
-           i番目の要素はi番目のアイテムの適合度を表す.
+    params
+    ------
+    rels :
+        array-like, shape = (n_entities, )
+        i-th element represents the relevance score of i-th item.
 
-    returns
-    -------
-    dcg  : float
+    return
+    ------
+    dcg :
+        float, The value of DCG.
     """
-    numer = lambda x: (2 ** x - 1)
-    denom = lambda i: np.log2(1 + (i + 1))
-    elems = [numer(rel) / denom(idx) for idx, rel in enumerate(rels)]
-    return reduce(add, elems)
+    numer = _calc_numer(rels)
+    denom = _calc_denom(len(rels))
+    return (numer / denom).sum()
+
 
 def nDCG(rels, topk=None):
-    """nDCGを計算する関数.
+    """Calcurate nDCG based on given relevances of entities.
 
-    arguments
-    ---------
-    rels : array-like, shape (n, )
-           i番目の要素はi番目のアイテムの適合度を表す.
+    params
+    ------
+    rels :
+        array-like, shape (n, )
+        i-th element represents the relevance score of i-th item.
+    topk :
+        int, k of nDCG@`k`
+        default to None,
+        which means all of the relevance will be considerd.
 
-    returns
-    -------
-    ndcg : float
+    return
+    ------
+    ndcg :
+        float, The value of nDCG.
     """
-    # [START listの場合の型変換]
-    if type(rels) == list:
-        rels = np.array(rels)
-    # [END]
     opt_rels = np.sort(rels)[::-1]
     last_idx = -1 if topk is None else topk
     dcg_obs = DCG(rels[:last_idx])
     dcg_opt = DCG(opt_rels[:last_idx])
     return dcg_obs / dcg_opt
-
-if __name__ == "__main__":
-    arr = [2, 0, 1, 0, 1]
-    dcg = DCG(arr)
-    ndcg = nDCG(arr, topk=3)
-    assert dcg == 3.5 + (1 / np.log2(6))
-    assert ndcg == 3.5 / (3.5 + (1 / np.log2(3)))
-    print("arr = {}".format(arr))
-    print("DCG: {}".format(dcg))
-    print("nDCG: {}".format(ndcg))
